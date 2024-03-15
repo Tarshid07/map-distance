9a9d7334917bbd452f5dd15538e09a1fe2dd9ac9
tarshid <akhtartarshid@gmail.com>
2024-03-15 13:51:42 +0530

    fix map

--- src/Map.js
+++ src/Map.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useDispatch } from "react-redux";
-import { addPoint, removePoint } from "./store";
+import { addPoint } from "./store";
 
 const Map = () => {
   const dispatch = useDispatch();
   const [map, setMap] = useState(null);
   mapboxgl.accessToken =
-    "pk.eyJ1IjoibWsxMDM5OSIsImEiOiJjbGdyc3RzaGcwNjBjM2xxMjgwZmJraGp4In0.nMEi2-CIuCNjHWQhnWeFPw";
-
+    "pk.eyJ1IjoidGFyc2hpZDA3IiwiYSI6ImNsdHNjZmlhejBzMHgycXFtc3J0eXNjMnMifQ.JeJEzJfmKDAOwPB7hKhLEQ&zoomwheel";
   useEffect(() => {
     const newMap = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/mk10399/clgs6hni5001u01pr2p8sa28y",
+      style: "mapbox://styles/tarshid07/cltsdt99t00go01qw3t3mfn9y",
       center: [77.21, 28.64],
       zoom: 12,
     });
@@ -22,9 +21,6 @@ const Map = () => {
 
   useEffect(() => {
     if (map) {
-      const distanceContainer = document.getElementById("distance");
-
-      // GeoJSON object to hold our measurement features
       const geojson = {
         type: "FeatureCollection",
         features: [],
@@ -78,25 +74,9 @@ const Map = () => {
           filter: ["in", "$type", "LineString"],
         });
        map.on("click", (e) => {
-         const features = map.queryRenderedFeatures(e.point, {
-           layers: ["measure-points"],
-         });
-
-          // Remove the linestring from the group
-          // so we can redraw it based on the points collection.
+        
           if (geojson.features.length > 1) geojson.features.pop();
 
-          // Clear the distance container to populate it with a new value.
-          distanceContainer.innerHTML = "";
-
-          // If a feature was clicked, remove it from the map.
-          if (features.length) {
-            const id = features[0].properties.id;
-            dispatch(removePoint(id));
-            geojson.features = geojson.features.filter(
-              (point) => point.properties.id !== id
-            );
-          } else {
             const point = {
               type: "Feature",
               geometry: {
@@ -110,9 +90,7 @@ const Map = () => {
 
             geojson.features.push(point);
             dispatch(addPoint(point));
-          }
 
-        //  if (geojson.features.length > 1) {
            linestring.geometry.coordinates = geojson.features.map(
               (point) => point.geometry.coordinates
           );
@@ -123,23 +101,12 @@ const Map = () => {
           map.getSource("geojson").setData(geojson);
         });
 
-        map.on("mousemove", (e) => {
-          const features = map.queryRenderedFeatures(e.point, {
-            layers: ["measure-points"],
-          });
-          // Change the cursor to a pointer when hovering over a point on the map.
-          // Otherwise cursor is a crosshair.
-          map.getCanvas().style.cursor = features.length
-            ? "pointer"
-            : "crosshair";
-        });
       });
     }
   }, [map]);
   return (
     <div>
       <div id="map" style={{ height: "100vh" }} />
-      <div id="distance" className="distance-container" />
     </div>
   );
 };
